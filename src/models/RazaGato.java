7174eddd83196a1c425101a28dab326ebba5160a
JuanPosso3 <juan.miguel.posso@correounivalle.edu.co>
2023-04-13 11:06:17 -0500

    Adicion de razas de gatos

--- src/models/RazaGato.java
+++ src/models/RazaGato.java
@@ -1,4 +1,16 @@
 package models;
 public enum RazaGato {
-    //TODO:Definir razas
+    Siames, Persa, GatoMontesEscoces, BritanicoDePeloCorto, MaineCoon, Esfinge, Bengali, Abisinio, DevonRex, AzulRuso, AmericanoDePeloCorto, Birmano, AngoraTurco, Siberiano, Criollo;
+    public String getNombreRaza(){
+        return name().replaceAll("([a-z][A-Z])","( )");
+    }
+
+    //Metodo que imprime las opciones de razas de gatos que estan disponibles con numeracion en lista
+    /* public void imprimirRazas(){
+        Integer i=1;
+        for(RazaGato raza: RazaGato.values()){
+        System.out.println(i+"."+raza.getNombreRaza());
+        i++;
+        }
+    } */
 }
