535dfdf68b691b6fd48211daa8f962e099bb9d6e
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2023-04-17 19:18:34 -0500

    Adicion de razas

--- src/models/RazaPerro.java
+++ src/models/RazaPerro.java
@@ -1,6 +1,20 @@
 package models;
 public enum RazaPerro {
-   Pug, Pinche, Doberman, PastorAleman
+      
+   AkitaInu, BassetHound, Beagle, BichonFrise, BichonMaltes, Bloodhound, BorderCollie, BostonTerrier, Boxer, 
+   BoyeroDeBerna, BulldogFrances, BulldogIngles, Bullmastiff, CairnTerrier, CaneCorso, Caniche, Carlino,
+   CavalierKingCharlesSpaniel, ChesapeakeBayRetriever, Chihuahua, ChowChow, CockerSpaniel, Collie, Dalmata, 
+   Dachshund, DobermanPinscher, DogoArgentino, DogoDeBurdeos,DogoDelTibet,DogoAleman,GoldenRetriever,GranPirineo,
+   Greyhound,GranDanes,HuskySiberiano,JackRussellTerrier,LabradorRetriever,LhasaApso,MastinNapolitano,MastinEspanol,
+   Maltipoo,Papillon,PastorAleman,PastorAustraliano,PastorBelga,PastorDeAnatolia,Pequines,PinscherMiniatura,
+   PitbullTerrier,Pointer,Pomerania,Pug,Rottweiler,SanBernardo,Schnauzer,SetterIrlandes,SharPei,ShihTzu,
+   SpringerSpaniel,StaffordshireBullTerrier,Terranova,Teckel,Weimaraner,WestHighlandWhiteTerrier,Whippet,
+   YorkshireTerrier;
+  
+   public String getNombreRaza(){
+      return name().replaceAll("(\\p{L1})(\\p{Lu})","$1 $2");  
+   }
+   
 }
 
 
