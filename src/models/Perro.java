a87915d138461dcd87eebbf75fa5817300ce7a6e
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2023-04-17 19:31:27 -0500

    Cambios

--- src/models/Perro.java
+++ src/models/Perro.java
@@ -3,7 +3,7 @@ public class Perro extends Mascota{
     
     RazaPerro raza;
     
-    public Perro(String paisOrige, double costo, String nombre, byte edad){
+    public Perro(Paises paisOrige, double costo, String nombre, byte edad){
         super(paisOrige, costo, nombre, edad);
        
     }
